859804133d9223285e40069879135b6dc2eb1a8b
Vilius Juodžiukynas <30282958+viliusddd@users.noreply.github.com>
2024-06-30 16:31:27 +0300

    feat: add initial config (#1)
    add initial eslint, prettier, nvm configs

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+// eslint.config.js
+import globals from "globals";
+
+export default [
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser
+            }
+        }
+    }
+];
