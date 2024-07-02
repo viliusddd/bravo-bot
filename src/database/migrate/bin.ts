23905acdf9aacef0e42ad7047481b7625e8f547d
Vilius <vilius@vilius.net>
2024-07-02 11:27:02 +0300

    feat: add migration execution file

---
+++ src/database/migrate/bin.ts
@@ -0,0 +1,43 @@
+import 'dotenv/config'
+import * as path from 'path'
+import {promises as fs} from 'fs'
+import {Kysely, Migrator, FileMigrationProvider, SqliteDialect} from 'kysely'
+import SQLite, {type Database} from 'better-sqlite3'
+
+const {DB_URL} = process.env
+
+async function migrateToLatest(url: string) {
+  const db = new Kysely<Database>({
+    dialect: new SqliteDialect({
+      database: new SQLite(url)
+    })
+  })
+
+  const migrator = new Migrator({
+    db,
+    provider: new FileMigrationProvider({
+      fs,
+      path,
+      migrationFolder: path.join(__dirname, '../migrations')
+    })
+  })
+
+  const {error, results} = await migrator.migrateToLatest()
+
+  results?.forEach(it => {
+    if (it.status === 'Success')
+      console.log(`migration "${it.migrationName}" was executed successfully`)
+    else if (it.status === 'Error')
+      console.error(`failed to execute migration "${it.migrationName}"`)
+  })
+
+  if (error) {
+    console.error('failed to migrate')
+    console.error(error)
+    process.exit(1)
+  }
+
+  await db.destroy()
+}
+
+migrateToLatest(DB_URL)
