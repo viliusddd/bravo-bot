f33ab9242697716169dbf54b830adc325738bffa
Vilius <vilius@vilius.net>
2024-07-02 17:23:23 +0300

    fix: print db url in gh action

--- src/app.test.ts
+++ src/app.test.ts
@@ -3,11 +3,10 @@ import supertest from 'supertest'
 import createApp from './app'
 import createDB from './database'
 
-const {DATABASE_URL} = process.env
-if (!DATABASE_URL) throw new Error('Provide DATABASE_URL in your env variable.')
-
-const db = createDB(DATABASE_URL)
+const db = createDB(process.env.DATABASE_URL)
 const app = createApp(db)
+
+console.log('db url: ', process.env.DATABASE_URL)
 
 describe('GET /messages', () => {
   it('should respond with a 200 status code', async () => {
