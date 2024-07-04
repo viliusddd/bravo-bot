072136d8468a49d707a8aba2adbebeb4005751f5
Vilius Juodžiukynas <30282958+viliusddd@users.noreply.github.com>
2024-07-04 18:42:56 +0300

    feat: add db seed data, adjust tables structure (#3)

--- src/database/migrations/20240629T133832Z-create-init-tables.ts
+++ src/database/migrations/20240629T133832Z-create-init-tables.ts
@@ -1,31 +1,55 @@
-import {Kysely} from 'kysely'
+import {type Kysely, sql} from 'kysely'
 
 export async function up(db: Kysely<any>) {
   await db.schema
     .createTable('users')
     .addColumn('id', 'integer', c => c.notNull().primaryKey().autoIncrement())
-    .addColumn('name', 'text', c => c.notNull())
+    .addColumn('username', 'text', c => c.notNull().unique())
     .execute()
 
   await db.schema
     .createTable('sprints')
-    .addColumn('id', 'integer', c => c.notNull().primaryKey().autoIncrement())
-    .addColumn('name', 'text', c => c.notNull())
+    .addColumn('id', 'text', c => c.notNull().primaryKey())
+    .addColumn('title', 'text', c => c.notNull().unique())
     .execute()
 
   await db.schema
     .createTable('messages')
     .addColumn('id', 'integer', c => c.notNull().primaryKey().autoIncrement())
-    .addColumn('message', 'text', c => c.notNull())
-    .addColumn('user_id', 'integer', c => c.notNull().references('users.id'))
+    .addColumn('message', 'text', c => c.notNull().unique())
+    .execute()
+
+  await db.schema
+    .createTable('templates')
+    .addColumn('id', 'integer', c => c.notNull().primaryKey().autoIncrement())
+    .addColumn('template', 'text', c => c.notNull().unique())
+    .execute()
+
+  await db.schema
+    .createTable('shoutouts')
+    .addColumn('id', 'integer', c => c.notNull().primaryKey().autoIncrement())
+    .addColumn('user_id', 'integer', c =>
+      c.notNull().references('users.id').onDelete('cascade')
+    )
     .addColumn('sprint_id', 'integer', c =>
-      c.notNull().references('sprints.id')
+      c.notNull().references('sprints.id').onDelete('cascade')
+    )
+    .addColumn('message_id', 'integer', c =>
+      c.notNull().references('messages.id').onDelete('cascade')
+    )
+    .addColumn('template_id', 'integer', c =>
+      c.notNull().references('templates.id').onDelete('cascade')
+    )
+    .addColumn('timestamp', 'text', c =>
+      c.defaultTo(sql`CURRENT_TIMESTAMP`).notNull()
     )
     .execute()
 }
 
 export async function down(db: Kysely<any>) {
   await db.schema.dropTable('users').execute()
-  await db.schema.dropTable('sprint').execute()
+  await db.schema.dropTable('sprints').execute()
   await db.schema.dropTable('messages').execute()
+  await db.schema.dropTable('templates').execute()
+  await db.schema.dropTable('shoutouts').execute()
 }
