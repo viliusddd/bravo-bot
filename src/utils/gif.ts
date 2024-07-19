478a95578ec2f3ecca2001f6c29b29744e6ccf48
Vilius <vilius@vilius.net>
2024-07-19 18:18:02 +0300

    feat: add gif fetch function

---
+++ src/utils/gif.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config'
+
+export default async function (): Promise<string | undefined> {
+  const url = `https://api.giphy.com/v1/gifs/random?${new URLSearchParams({
+    api_key: process.env.GIPHY_API_KEY,
+    tag: 'celebrate',
+    rating: 'g'
+  })}`
+
+  try {
+    const response = await fetch(url)
+
+    if (!response.ok) throw new Error(`Response status: ${response.status}`)
+
+    const {data, errors} = await response.json()
+
+    if (errors) throw new Error(errors.message)
+
+    return data.images.original.url
+  } catch (err) {
+    console.error('Promise rejected', err)
+    return undefined
+  }
+}
