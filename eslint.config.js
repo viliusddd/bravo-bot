da28ad0cb27da1c424deeabafdeeafa1940c3e2e
Vilius <vilius@vilius.net>
2024-07-31 16:14:16 +0300

    build: bump eslint ver, add new ver eslint config

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import airbnb from 'eslint-config-airbnb-base'
+import airbnbTS from 'eslint-config-airbnb-typescript'
+import prettier from 'eslint-config-prettier'
+
+export default [
+  airbnb,
+  airbnbTS,
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    rules: {
+      // we can override some problematic import rules here
+      // that can cause issues when using import aliases.
+      'import/extensions': 'off',
+      'import/no-extraneous-dependencies': 'off',
+
+      // functions are always hoisted, so we can use them before they are defined
+      // which in various cases improves readability
+      'no-use-before-define': ['error', {functions: false}],
+      '@typescript-eslint/no-use-before-define': ['error', {functions: false}],
+
+      'import/prefer-default-export': 'off',
+      'no-console': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/no-unknown-property': 'off'
+    }
+  },
+  {
+    languageOptions: {globals: globals.browser}
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier
+]
